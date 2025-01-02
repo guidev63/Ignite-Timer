fc37cb2c0c0c940d2d1bba036460021aa5618173
Guilherme Augusto <guilhermedev23@gmail.com>
2025-01-02 16:44:28 -0300

    ok

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,14 @@
+//definição de tipos //
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default'
+
+
+type Themetype = typeof defaultTheme;
+
+declare module 'styled-components'{
+    export interface DefaultTheme extends  Themetype {
+     
+        
+      
+    }
+}
