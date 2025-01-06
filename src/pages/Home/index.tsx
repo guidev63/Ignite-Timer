94afd4b20191e117d15d072e9d63d6c1d11aef62
Guilherme Augusto <guilhermedev23@gmail.com>
2025-01-06 00:06:45 -0300

    ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,10 +8,11 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
-import { useForm, useFormState } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from 'date-fns';
 
 const newCycleformValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -24,15 +25,17 @@ const newCycleformValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleformValidationSchema>;
 
 interface Cycle {
-  id: string;
-  task: string;
-  minutesAmount: number;
-  isActive: boolean;
+  id: string
+  task: string
+  minutesAmount: number
+  isActive: boolean
+  startDate: Date
 }
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset, formState } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleformValidationSchema),
@@ -41,9 +44,25 @@ export function Home() {
       minutesAmount: 0,
     },
   });
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    let interval:number 
+    if (activeCycle) {
+       interval = setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000);
+    }
+    return() =>{
+      clearInterval(interval)
+    }
+  }, [activeCycle])
+
+
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log("Form data received:", data); // Log para debug
+    console.log("Form data received:", data); 
 
     const id = String(new Date().getTime());
 
@@ -52,18 +71,36 @@ export function Home() {
       task: data.task,
       minutesAmount: data.minutesAmount,
       isActive: true,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
 
     setActiveCycleId(id);
-
-    console.log("Novo ciclo criado:", newCycle); // Log para verificar o ciclo criado
+    setAmountSecondsPassed(0);
+    console.log("Novo ciclo criado:", newCycle); 
     reset();
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
+
+
+
+  useEffect(()=>{
+    if (activeCycle){
+
+    document.title =`${minutes}:${seconds}`
+    }
+  },[minutes,seconds,activeCycle])
   const task = watch("task");
   const isSubmitDisabled = !task;
 
@@ -96,18 +133,18 @@ export function Home() {
             step={5}
             min={5}
             max={60}
-            {...register("minutesAmount", { valueAsNumber: true })}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>Minutos</span>
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
